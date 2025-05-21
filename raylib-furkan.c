ad40dab36524841ca14f0f238a6bba0d5ff063fe
furkanp01 <gm4iliyedim@gmail.com>
2025-05-21 15:41:43 +0300

    Update raylib-furkan.c

--- raylib-furkan.c
+++ raylib-furkan.c
@@ -12,10 +12,10 @@
 #define gameHeight 1280
 #define gridSize 8
 #define cellSize 100
-#define boardoffsetX 100
-#define boardoffsetY 150
-#define candyTypes 5
+#define candyTypes 6
 #define framecount 8
+#define maxLevels 5
+
 
 
 
@@ -27,6 +27,7 @@ typedef enum {
 	GAMEOVER,
 	WIN,
 	LEVELUP,
+	QUIT
 }gameState;
 
 //Candy types
@@ -57,22 +58,23 @@ typedef enum {
 
 //Level structure
 typedef struct {
+	int levelId;
 	int targetScore;
 	int maxMoves;
-	float timeLimit;
-	int requiredspecialCandies;
+	bool isUnlocked;
 }levelState;
 
 //Candy structure
 typedef struct {
 	int baseType;
 	Vector2 position;
-	Vector2 targerPosition;
+	Vector2 targetPosition;
 	Vector2 startPosition;
 	float animTime;
 	float scale;
 	float alpha;
 	animationState animState;
+	bool hasCandy;
 }candyState;
 
 
@@ -80,12 +82,10 @@ typedef struct {
 typedef struct {
 
 	candyState gameBoard[gridSize][gridSize];
-	levelState levels[5];
 	gameState state;
 
 	int score;
 	int moves;
-	int currentLevel;
 	int specialcandiesCreated;
 	int selectedRow;
 	int selectedColumn;
@@ -94,20 +94,24 @@ typedef struct {
 	bool isAnimating;
 	bool isSwapping;
 	bool isgameOver;
-	bool hasSelected;
 	bool soundOn;
 	bool showmenuSettings;
 	bool showlevelSettings;
 	bool showgameSettings;
 	bool confirmQuit;
+	bool boardInitialized;
+	bool checkMatches;
 
 	float animationTimer;
 	float gameTime;
+	float cellWidth;
+	float cellHeight;
+	float boardoffsetX;
+	float boardoffsetY;
 
 	Texture2D candyTextures[candyTypes];
 	Texture2D specialTextures[4];
-	Texture2D backgroundWp, levelWp, backIcon, settingsIcon;
-	Texture2D red;
+	Texture2D backgroundWp, levelWp, backIcon, settingsIcon, gameboardWp;
 	Texture2D menu[framecount];
 	Sound swapSound;
 	Sound matchSound;
@@ -117,9 +121,19 @@ typedef struct {
 
 	Font myFont;
 
-}gameBoard;
+}gameResources;
 
-gameBoard resources;
+gameResources resources;
+
+void initgameBoard();
+void drawmenuScreen();
+void loadlevelData();
+void savelevelData();
+void completeLevel(int level);
+void drawlevelScreen();
+void drawgameScreen();
+void ToggleSound();
+void unloadRes();
 
 //Initialize resources
 void initRes() {
@@ -131,6 +145,7 @@ void initRes() {
 	resources.candyTextures[4] = LoadTexture("resources/candy4.png");
 	resources.candyTextures[5] = LoadTexture("resources/candy5.png");
 	resources.backgroundWp = LoadTexture("resources/background.png");
+	resources.gameboardWp = LoadTexture("resources/gameBoardWp.png");
 	resources.menu[0] = LoadTexture("resources/menu.png");
 	resources.menu[1] = LoadTexture("resources/menu1.png");
 	resources.menu[2] = LoadTexture("resources/menu2.png");
@@ -299,6 +314,50 @@ void drawmenuScreen(void) {
 	}
 }
 
+bool isUnlocked[maxLevels] = { true,false,false,false,false };
+
+void loadlevelData() {
+	FILE* xPtr = fopen("levels.txt", "r");
+	if (xPtr == NULL) {
+		printf("File couldn't be opened.");
+		currentState = QUIT;
+	}
+	else
+	{
+		for (int i = 0;i < maxLevels;i++) {
+			int val;
+			if (fscanf_s(xPtr, "%d", &val) == 1) {
+				isUnlocked[i] = val;
+			}
+		}
+		fclose(xPtr);
+	}
+
+}
+
+void savelevelData() {
+	FILE* xPtr = fopen("levels.txt", "w");
+	if (xPtr == NULL) {
+		printf("The file couldn't be opened.");
+		currentState = QUIT;
+	}
+	else {
+		for (int i = 0;i < maxLevels;i++) {
+			fprintf(xPtr, "%d\n", isUnlocked[i]);
+		}
+		fclose(xPtr);
+	}
+}
+
+void completeLevel(int level) {
+	if (level + 1 < maxLevels) {
+		isUnlocked[level + 1] = true;
+	}
+	savelevelData();
+}
+
+int currentLevel = 0;
+
 
 
 void drawlevelScreen(void) {
@@ -334,25 +393,36 @@ void drawlevelScreen(void) {
 	);
 
 
-	int buttonRadius = 40;
-	int buttonSpacing = 30;
+	int buttonRadius = 25;
+	int buttonSpacing = 20;
 	int totalHeight = 5 * buttonRadius * 2 + 4 * buttonSpacing;
 	int startY = (currentScreenHeight - totalHeight) / 2 + buttonRadius;
 	int centerX = currentScreenWidth / 2;
 
-	for (int i = 0; i < 5; i++) {
-		int levelNum = 5 - i;
+	for (int i = 0; i < maxLevels; i++) {
 		int cy = startY + i * (buttonRadius * 2 + buttonSpacing);
 
-		DrawCircle(centerX, cy, buttonRadius, LIGHTGRAY);
-		DrawCircleLines(centerX, cy, buttonRadius, DARKGRAY);
+		Color fillColor = isUnlocked[i] ? (Color) { 173, 216, 230, 255 } : GRAY;
+		DrawCircle(centerX, cy, buttonRadius, fillColor);
+		DrawCircleLines(centerX, cy, buttonRadius, BLACK);
 
 		char label[2];
-		snprintf(label, sizeof(label), "%d", levelNum);
+		snprintf(label, sizeof(label), "%d", i + 1);
 
-		int fontSize = 32;
+		int fontSize = 30;
 		int textWidth = MeasureText(label, fontSize);
 		DrawText(label, centerX - textWidth / 2, cy - fontSize / 2, fontSize, BLACK);
+
+
+		if (isUnlocked[i]) {
+			if (CheckCollisionPointCircle(GetMousePosition(), (Vector2) { centerX, cy }, buttonRadius)) {
+				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+					currentLevel = i;
+					currentState = GAME;
+					initgameBoard();
+				}
+			}
+		}
 	}
 
 
@@ -426,9 +496,10 @@ void drawlevelScreen(void) {
 		DrawTextEx(myFont, "Sound On/Off", (Vector2) { soundButton.x + 10, soundButton.y + 8 }, fontSize, spacing, BLACK);
 
 		// Çıkış butonu
-		Rectangle quitButton = { panel.x + 50, panel.y + 130, 200, 40 };
-		DrawRectangleRounded(quitButton, 0.3f, 10, RED);
-		DrawTextEx(resources.myFont, "Quit Game", (Vector2) { quitButton.x + 10, quitButton.y + 8 }, fontSize, spacing, BLACK);
+		Rectangle backBtn = { panel.x + panel.width - 110, panel.y + panel.height - 60, 100, 40 };
+		DrawRectangleRec(backBtn, LIGHTGRAY);
+		DrawRectangleLines(backBtn.x, backBtn.y, backBtn.width, backBtn.height, BLACK);
+		DrawText("Back", backBtn.x + 30, backBtn.y + 10, 20, BLACK);
 
 		Vector2 mouse = GetMousePosition();
 		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
@@ -436,14 +507,266 @@ void drawlevelScreen(void) {
 				resources.soundOn = !resources.soundOn;
 				SetMasterVolume(resources.soundOn ? 1.0f : 0.0f);
 			}
-			if (CheckCollisionPointRec(mouse, quitButton)) {
-				CloseWindow();  // Oyunu kapat
+			if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+				Vector2 mouse = GetMousePosition();
+				if (CheckCollisionPointRec(mouse, backBtn)) {
+					resources.showlevelSettings = false;
+				}
 			}
 		}
 	}
 }
 
+int gameBoard[gridSize][gridSize];
 
+Vector2 gridOffset;
+bool hasSelected = false;
+Vector2 selectedCell = { -1,-1 };
+
+void updateGridOffset() {
+	int currentscreenWidth = GetScreenWidth();
+	int currentscreenHeight = GetScreenHeight();
+	gridOffset.x = (currentscreenWidth - (gridSize * cellSize)) / 2;
+	gridOffset.y = (currentscreenHeight - (gridSize * cellSize)) / 2;
+}
+
+void initgameBoard() {
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			int validTypes[candyTypes];
+			int validCount = 0;
+
+			for (int type = 0; type < candyTypes; type++) {
+				bool valid = true;
+
+				if (j >= 2 &&
+					resources.gameBoard[i][j - 1].baseType == type &&
+					resources.gameBoard[i][j - 2].baseType == type) {
+					valid = false;
+				}
+
+				if (i >= 2 &&
+					resources.gameBoard[i - 1][j].baseType == type &&
+					resources.gameBoard[i - 2][j].baseType == type) {
+					valid = false;
+				}
+
+				if (valid) {
+					validTypes[validCount++] = type;
+				}
+			}
+
+			int selected = validTypes[rand() % validCount];
+			resources.gameBoard[i][j].baseType = selected;
+			resources.gameBoard[i][j].position.x = j * cellSize;
+			resources.gameBoard[i][j].position.y = i * cellSize;
+		}
+	}
+	updateGridOffset();
+}
+
+void drawCandies() {
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			int type = resources.gameBoard[i][j].baseType;
+			if (type >= 0 && type < candyTypes) {
+				Texture2D tex = resources.candyTextures[type];
+				float offsetX = (cellSize - tex.width) / 2.0f;
+				float offsetY = (cellSize - tex.height) / 2.0f;
+
+				DrawTexture(
+					tex,
+					(int)(resources.gameBoard[i][j].position.x + gridOffset.x + offsetX),
+					(int)(resources.gameBoard[i][j].position.y + gridOffset.y + offsetY),
+					WHITE
+				);
+			}
+		}
+	}
+}
+
+void drawgridLines() {
+	Color boxColor = Fade(GRAY, 0.3f);
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			Rectangle cellRect = {
+				gridOffset.x + j * cellSize,
+				gridOffset.y + i * cellSize,
+				cellSize,
+				cellSize
+			};
+			DrawRectangleRoundedLines(cellRect, 0.2f, 6, boxColor);
+		}
+	}
+	updateGridOffset();
+}
+
+Vector2 getCellFromMouse(Vector2 mousePos) {
+	return (Vector2) {
+		(int)((mousePos.x - gridOffset.x) / cellSize),
+			(int)((mousePos.y - gridOffset.y) / cellSize)
+	};
+}
+
+void swapCandies(int row1, int col1, int row2, int col2) {
+	candyState temp = resources.gameBoard[row1][col1];
+	resources.gameBoard[row1][col1] = resources.gameBoard[row2][col2];
+	resources.gameBoard[row2][col2] = temp;
+
+	resources.gameBoard[row1][col1].position = (Vector2){ col1 * cellSize, row1 * cellSize };
+	resources.gameBoard[row2][col2].position = (Vector2){ col2 * cellSize, row2 * cellSize };
+}
+
+
+void selectCandy() {
+	if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+		Vector2 mousePos = GetMousePosition();
+		Vector2 clickedCell = getCellFromMouse(mousePos);
+
+		if (clickedCell.x >= 0 && clickedCell.x < gridSize &&
+			clickedCell.y >= 0 && clickedCell.y < gridSize) {
+
+			if (!hasSelected) {
+				selectedCell = clickedCell;
+				hasSelected = true;
+			}
+			else {
+				int dx = (int)(clickedCell.x - selectedCell.x);
+				int dy = (int)(clickedCell.y - selectedCell.y);
+
+				if ((abs(dx) == 1 && dy == 0) || (abs(dy) == 1 && dx == 0)) {
+					swapCandies((int)selectedCell.y, (int)selectedCell.x, (int)clickedCell.y, (int)clickedCell.x);
+				}
+
+				hasSelected = false;
+			}
+		}
+	}
+}
+
+bool checkMatches() {
+	bool matchFound = false;
+
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize - 2; j++) {
+			int type = resources.gameBoard[i][j].baseType;
+			if (type >= 0 &&
+				type == resources.gameBoard[i][j + 1].baseType &&
+				type == resources.gameBoard[i][j + 2].baseType) {
+
+				resources.gameBoard[i][j].baseType = -1;
+				resources.gameBoard[i][j + 1].baseType = -1;
+				resources.gameBoard[i][j + 2].baseType = -1;
+				matchFound = true;
+			}
+		}
+	}
+
+	for (int j = 0; j < gridSize; j++) {
+		for (int i = 0; i < gridSize - 2; i++) {
+			int type = resources.gameBoard[i][j].baseType;
+			if (type >= 0 &&
+				type == resources.gameBoard[i + 1][j].baseType &&
+				type == resources.gameBoard[i + 2][j].baseType) {
+
+				resources.gameBoard[i][j].baseType = -1;
+				resources.gameBoard[i + 1][j].baseType = -1;
+				resources.gameBoard[i + 2][j].baseType = -1;
+				matchFound = true;
+			}
+		}
+	}
+
+	return matchFound;
+}
+
+void dropCandies() {
+	for (int j = 0; j < gridSize; j++) {
+		for (int i = gridSize - 1; i >= 0; i--) {
+			if (resources.gameBoard[i][j].baseType == -1) {
+				for (int k = i - 1; k >= 0; k--) {
+					if (resources.gameBoard[k][j].baseType != -1) {
+						resources.gameBoard[i][j].baseType = resources.gameBoard[k][j].baseType;
+						resources.gameBoard[k][j].baseType = -1;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+void refillBoard() {
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			if (resources.gameBoard[i][j].baseType == -1) {
+				resources.gameBoard[i][j].baseType = rand() % candyTypes;
+			}
+		}
+	}
+}
+
+void removeMatches() {
+	bool chainReaction = true;
+	while (chainReaction) {
+		chainReaction = false;
+		if (checkMatches()) {
+			dropCandies();
+			refillBoard();
+			chainReaction = true;
+			resources.score += 100;
+		}
+	}
+}
+
+void drawgameScreen() {
+	DrawTexture(resources.backgroundWp, 0, 0, WHITE);
+
+	int currentScreenWidth = GetScreenWidth();
+	int currentScreenHeight = GetScreenHeight();
+	float scale = (float)currentScreenHeight / (float)gameHeight;
+	int drawWidth = (int)(gameWidth * scale);
+	int drawHeight = currentScreenHeight;
+	int offsetX = (currentScreenWidth - drawWidth) / 2;
+
+	DrawTexturePro(
+		resources.gameboardWp,
+		(Rectangle) {
+		0, 0, (float)resources.gameboardWp.width, (float)resources.gameboardWp.height
+	},
+		(Rectangle) {
+		offsetX, 0, drawWidth, drawHeight
+	},
+		(Vector2) {
+		0, 0
+	}, 0.0f, WHITE
+	);
+
+	drawgridLines();
+	drawCandies();
+	selectCandy();
+
+	if (hasSelected) {
+		Rectangle selectedRect = {
+			gridOffset.x + selectedCell.x * cellSize,
+			gridOffset.y + selectedCell.y * cellSize,
+			cellSize,
+			cellSize
+		};
+		DrawRectangleLinesEx(selectedRect, 3, RED);
+	}
+
+	static bool shouldCheckMatches = false;
+	if (!shouldCheckMatches && !hasSelected && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+		shouldCheckMatches = true;
+	}
+
+	if (shouldCheckMatches) {
+		removeMatches();
+		resources.moves--;
+		shouldCheckMatches = false;
+	}
+}
 
 
 
@@ -459,30 +782,15 @@ void ToggleSound() {
 	}
 }
 
-void settings(void) {
 
-
-
-
-
-
-}
-
-
-
-void drawgameScreen() {
-
-
-
-
-
-}
 
 
 //Unload resources
 void unloadRes(void) {
 	UnloadTexture(resources.backgroundWp);
-	UnloadTexture(resources.menu[framecount]);
+	for (int i = 0; i < framecount; i++) {
+		UnloadTexture(resources.menu[i]);
+	}
 	UnloadTexture(resources.levelWp);
 	UnloadMusicStream(resources.music);
 	UnloadFont(resources.myFont);
